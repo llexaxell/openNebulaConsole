40e3ba73f5f2abd3134ea60c728fdd344da4630c
llexaxell <guillermeaxel@yahoo.fr>
2013-11-24 08:22:34 -0800

    Init repo

---
+++ src/view/main.java
@@ -0,0 +1,18 @@
+package view;
+
+/**
+ * 
+ * @author AxelG
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("b");
+
+	}
+
+}
